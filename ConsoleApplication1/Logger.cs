732aeb4f6587b1b92697aaeec93ceb0bf3bd9333
Admin <Admin@DESKTOP-7SJFEEA>
2017-06-27 16:41:51 +0300

    Add project files.

---
+++ ConsoleApplication1/Logger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApplication1
+{
+    class Logger
+    {
+        private StreamWriter lf;
+        private StreamWriter elf;
+
+        public Logger(string logFile, string errorLogFile)
+        {
+            if (!File.Exists(logFile) || !File.Exists(errorLogFile))
+            {
+                //Attempt to create a file
+                string path = Path.GetDirectoryName(logFile);
+                if (!Directory.Exists(path)) { Directory.CreateDirectory(path); }
+                File.Create(logFile);
+                File.Create(errorLogFile);
+            }
+            //Console.WriteLine(logFile + errorLogFile);
+            lf = new StreamWriter(logFile, true);
+            elf = new StreamWriter(errorLogFile, true);
+            lf.AutoFlush = true;
+            elf.AutoFlush = true;
+
+        }
+
+        public async void WriteLog(string Message)
+        {
+            if (lf.BaseStream.CanWrite == false) { System.Windows.Forms.MessageBox.Show("Cant Write to log"); }
+            await lf.WriteLineAsync("[" + DateTime.Now + "]" + " " + Message);
+
+            //this.lf.WriteLineAsync("[{0}] {1}", DateTime.Now, Message);
+        }
+        public async void WriteError(string Message)
+        {
+            if (elf.BaseStream.CanWrite == false) { System.Windows.Forms.MessageBox.Show("Cant Write to error log"); }
+            await elf.WriteLineAsync("[" + DateTime.Now + "]" + " " + Message);
+            //this.elf.WriteLineAsync("[{0}] {1}", DateTime.Now, Message);
+        }
+        public void Destructor()
+        {
+            WriteError("Error logging ends");
+            WriteLog("Logging ends");
+            if (this.lf.BaseStream.CanRead == true) { this.lf.Dispose(); }
+            if (this.elf.BaseStream.CanRead == true) { this.elf.Dispose(); }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurationMap = new Dictionary<string, string>();
+            string workingDir = Directory.GetCurrentDirectory();
+            string configFile = "C:\\Users\\Admin\\Documents\\Visual Studio 2015\\Projects\\ConsoleApplication1\\ConsoleApplication1\\bin\\Debug\\logon.cfg";
+            if (File.Exists(configFile))
+            {
+                string[] filecontent = File.ReadAllLines(configFile);
+                foreach (string line in filecontent)
+                {
+                    char delimiter = '=';
+                    string[] lineValues = line.Split(delimiter);
+                    configurationMap.Add(lineValues[0], lineValues[1]);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Configuration file does not exist. Exiting.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Initiating Logger
+            Logger Log = new Logger(configurationMap["logFile"], configurationMap["errorLog"]);
+            Log.WriteLog("Logging beggins");
+            Log.WriteError("Error logging beggins");
+
+            Log.Destructor(); //Finish logging and flush everything
+        }
+    }
+}
